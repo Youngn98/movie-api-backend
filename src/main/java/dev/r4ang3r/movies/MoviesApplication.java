2db9fc357540beebd0ac31bb93a65e9a03334069
r4ang3r <111329962+r4ang3r@users.noreply.github.com>
2023-05-17 18:52:33 -0500

    Add files via upload

---
+++ src/main/java/dev/r4ang3r/movies/MoviesApplication.java
@@ -0,0 +1,14 @@
+package dev.r4ang3r.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+
+	}
+
+}
---
+++ src/main/java/dev/r4ang3r/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.r4ang3r.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
